563067b5f936cf734fd622fdccefd0fe9d302eb9
azizbek400 <aziznormurodov400@gmail.com>
2024-02-28 21:29:47 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+// INTEGER 14 /////////////////////
+
+// let c = 150;
+// console.log(c);
+// let result = parseInt(c / 10);
+// console.log(result);
+
+
+// INTEGER 15  ////////////////////
+
+// let a = 123;
+// let b = 213;
+// b -= a;
+// let c = 123 + b;
+
+// console.log(c);
+
+
+// INTEGER 16  ////////////////////
+
+// let a = 123;
+// let b = 132;
+// b -= a;
+// let c = 123 + b;
+
+// console.log(c);
+
+// INTEGER 17  ////////////////////
+
+// let a = 1201;
+// let b = a % 2;
+// console.log(b);
+
+
+// INTEGER 18  ////////////////////
+
+// let a = parseInt(2200 / 1000);
+// console.log(a);
+
+// INTEGER 19  ////////////////////
+
+
+
+
+
+
+
+
+
